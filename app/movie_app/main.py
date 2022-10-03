cfc21e46c096a31836c41366da6aabf5ffd2bde0
pascal.edouard@brambles.com <pascal.edouard@cloudreach.com>
2022-10-03 15:31:16 +0100

    moving postgresql

--- app/movie_app/main.py
+++ app/movie_app/main.py
@@ -1,18 +1,17 @@
 import json
 
 from flask import Flask, render_template
-from flask_mysql_connector import MySQL
+import psycopg2
 from flask import request
 
 app = Flask("MovieApp")
 
-app.config["MYSQL_HOST"] = "127.0.0.1"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "my-secret-pw" # TODO enter your supersecret password
-app.config["MYSQL_DATABASE"] = "movie_db"
-app.config["MYSQL_AUTH_PLUGIN"] = "mysql_native_password"
-
-mysql = MySQL(app)
+def get_db_connection():
+    conn = psycopg2.connect(host='127.0.0.1',
+                            database='movie_db',
+                            user="root",
+                            password="my-secret-pw")
+    return conn
 
 @app.route("/")
 # Just to test that flask is working with Hello World
@@ -21,25 +20,30 @@ def hello_world():
 
 @app.route("/movies/")
 def list_movies():
-    cursor = mysql.connection.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor()
     query_string = "SELECT * FROM movies_tbl;"
     cursor.execute(query_string)
     data = cursor.fetchall()
     cursor.close()
+    conn.close()
     return json.dumps(data) # Return data as a string
 
 @app.route("/movies-table/")
 def list_movie_table(): 
-    cursor = mysql.connection.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor()
     query_string = "SELECT * FROM movies_tbl;"
     cursor.execute(query_string)
     data = cursor.fetchall()
     cursor.close()
+    conn.close()
     return render_template("movies.html.tpl", movies_data=data) # Return data as rendered html template
 
 @app.route("/new-movie/")
 def new_movie(): 
-    cursor = mysql.connection.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor()
     d_query_string = "SELECT * FROM directors_tbl;"
     a_query_string = "SELECT * FROM main_actors_tbl;"
     cursor.execute(d_query_string)
@@ -47,6 +51,7 @@ def new_movie():
     cursor.execute(a_query_string)
     actors_data = cursor.fetchall()
     cursor.close()
+    conn.close()
     return render_template("add-movie.html.tpl",
         directors_data=directors_data,
         actors_data=actors_data
@@ -54,7 +59,8 @@ def new_movie():
 
 @app.route('/add-new-movie/', methods=['POST'])
 def add_new_movie():
-    cursor = mysql.connection.cursor()
+    conn = get_db_connection()
+    cursor = conn.cursor()
 
     # Collect info from Form
     title = request.form['movietitle']
@@ -70,8 +76,9 @@ def add_new_movie():
     # INSERT into the table movie_actors_tbl
     movie_actor_query_string = "INSERT INTO movie_actors_tbl VALUES(%s, %s);"
     cursor.execute(movie_actor_query_string, (new_movie_id, actor_id))
-    mysql.connection.commit()
+    conn.commit()
     cursor.close()
+    conn.close()
     return list_movie_table()
 
 
