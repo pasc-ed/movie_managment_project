21f583b7689f979d11953516e3986ef12c5cb8b7
pascal.edouard@brambles.com <pascal.edouard@cloudreach.com>
2022-10-03 17:53:27 +0100

    use user-data

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "movie_app_server" {
   subnet_id              = data.aws_subnet.public_subnet_a.id
   vpc_security_group_ids = [aws_security_group.movie_app_sg.id]
   key_name = var.keypair_name
-#  user_data = "${path.module}/user-data.sh"
+  user_data = "${path.module}/user-data.sh"
 
   tags = {
     Name = "movie-mgmt-server"
