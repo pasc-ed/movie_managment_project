a6d384998ce3053e3874a6736864cfa8a3863cc3
pascal.edouard@brambles.com <pascal.edouard@cloudreach.com>
2022-10-04 15:57:57 +0100

    adding user-data

--- infra/user-data.sh
+++ infra/user-data.sh
@@ -18,16 +18,23 @@ sudo apt-get update
 sudo apt-get install docker.io -y
 
 # CLONE MY MOVIE-MANGEMENT-PROJECT REPO
-git clone --branch demo2 https://github.com/pasc-ed/movie_managment_project.git
+git clone --branch demo2 https://github.com/pasc-ed/movie_managment_project.git ~/movie_managment_project
 
 # BUILD MY DOCKER IMAGE - DOCKERFILE
-cd movie_managment_project/app
+cd ~/movie_managment_project/app
 docker build -t movie-mgmt .
 
 # RUN MYSQL CONTAINER
 mkdir ~/database
-docker run --name movie-db-mysql -p 3306:3306 -v ~/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:latest --default-authentication-plugin=mysql_native_password
+docker run --name movie-db-mysql -p 3306:3306 -v ~/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:latest
+# --default-authentication-plugin=mysql_native_password
 
 # DEPLOY OUR DATABASE INSIDE THE MYSQL CONTAINER
+mysql -h 127.0.0.1 -u root -pmy-secret-pw < ~/movie_managment_project/database/create_movie_database.sql
+
+
+container_ip=`docker inspect movie-db-mysql | grep -e '"IPAddress"' -m 1|awk -F '"' '{print $4}'`
+sed -i "s/DOCKER_CONTAINER_IP_PLACEHOLDER/${container_ip}/g" ~/movie_managment_project/app/movie_app/main.py
+
 # RUN MY CONTAINER - FLASK APP RUNNING
-docker run -d -p 80:80 --name=movie-mgmt -v $PWD/movie_app:/app movie-mgmt
+docker run -d -p 80:80 --name=movie-mgmt -v ~/movie_managment_project/app/movie_app:/app movie-mgmt
